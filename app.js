625aad6b0e821d37d7ad4917c1902beb364b3efd
erikaCutugno <erikucciapop@gmail.com>
2025-02-05 15:37:31 +0100

    feat: add cors

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const cors = require("cors");
 const postsRouter = require("./routers/posts");
 const routeNotFound = require("./middlewares/routeNotFound");
 const errorsHandler = require("./middlewares/errorsHandler");
 
 app.use(express.static("public"));
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use("/posts", postsRouter);
 
 app.use(errorsHandler);
--- data/post.js
+++ data/post.js
@@ -2,7 +2,7 @@ const posts = [
   {
     id: 1,
     title: "Ciambellone",
-    image: `http:localhost:3001/images/ciambellone.jpeg`,
+    image: `https://media.istockphoto.com/id/1443993528/it/foto/pound-cake-tradizionale-aroma-di-vaniglia-o-panna-acida.jpg?s=2048x2048&w=is&k=20&c=NT4m0HwmIX6yahKy9iNiZHj0YR5pm_ozLaZ2f9CSniI=`,
     content:
       "Il ciambellone è il dolce ideale per ogni momento della giornata. Facile da preparare, soffice e goloso, rappresenta la dolcezza della cucina casalinga.",
     tags: ["Dolci", "Ciambellone", "Ricetta", "Tradizione", "Colazione"],
@@ -10,7 +10,7 @@ const posts = [
   {
     id: 2,
     title: "Cracker Barbabietola",
-    image: `http:localhost:3001/images/cracker_barbabietola.jpeg`,
+    image: `https://www.giallozafferano.it/images/267-26779/Cracker-alla-barbabietola_780x520_wm.jpg`,
     content:
       "Preparare i cracker in casa è più facile di quanto sembri! Croccanti, leggeri e perfetti come snack salato, sono un'ottima alternativa ai prodotti industriali.",
     tags: ["Snack", "Cracker", "Ricetta Facile", "Fatto in Casa", "Leggero"],
@@ -18,7 +18,7 @@ const posts = [
   {
     id: 3,
     title: "Pane Fritto Dolce",
-    image: `http:localhost:3001/images/pane_fritto_dolce.jpeg`,
+    image: `https://www.giallozafferano.it/images/267-26795/Pane-fritto-dolce_450x300.jpg`,
     content:
       "Il pane fritto è una preparazione semplice e golosa, ideale per accompagnare un piatto salato o per essere gustato come snack. Un'esplosione di sapore!",
     tags: ["Pane", "Fritto", "Snack", "Ricetta Facile", "Cucina Italiana"],
@@ -26,7 +26,7 @@ const posts = [
   {
     id: 4,
     title: "Pasta Barbabietola",
-    image: `http:localhost:3001/pasta_barbabietola.jpeg`,
+    image: `https://www.giallozafferano.it/images/183-18377/Pasta-con-barbabietola-e-pecorino_780x520_wm.jpg`,
     content:
       "La pasta alla barbabietola è un piatto ricco di sapore e dal colore vivace. Facile da preparare, aggiunge un tocco originale alla tua tavola, perfetta per sorprendere gli ospiti.",
     tags: [
@@ -40,7 +40,7 @@ const posts = [
   {
     id: 5,
     title: "Paesana",
-    image: `http:localhost:3001/images/paesana.jpeg`,
+    image: `https://www.giallozafferano.it/images/11-1118/Torta-paesana_780x520_wm.jpg`,
     content:
       "La paesana è una ricetta rustica e gustosa che racconta la tradizione della cucina contadina italiana. Un piatto che unisce sapori semplici ma straordinari.",
     tags: [
